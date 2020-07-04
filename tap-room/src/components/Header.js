7dee2e9c3debd514dee62d6c4bbafce31563dd16
brevin <cronkbrevin98@gmail.com>
2020-07-03 17:20:38 -0700

    Added some more styling to the webpage, trying to make the UX better

--- tap-room/src/components/Header.js
+++ tap-room/src/components/Header.js
@@ -3,8 +3,11 @@ import React from 'react';
 function Header() {
 	return (
 		<React.Fragment>
-			<h1>The Tap House</h1>
-			<h3>Welcome to the Tap House</h3>
+			<div id="header">
+				<h1 id="store">The Tap House</h1>
+				<h3 id="greeting">Welcome to the Tap House</h3>
+			</div>
+			<hr id="headbar" />
 		</React.Fragment>
 	);
 }
--- tap-room/src/components/Keg.js
+++ tap-room/src/components/Keg.js
@@ -2,14 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Keg(props) {
+	const { keg, onClickBuy } = props;
 	return (
 		<React.Fragment>
-			<div onClick={() => props.whenKegClicked(props.id)}>
+			<div onClick={() => props.whenKegClicked(props.id)} id="kegList">
 				<h3>Brand: {props.kegBrand}</h3>
 				<h3>Name: {props.kegName}</h3>
 				<h3>Price: {props.kegPrice}</h3>
 				<h3>ABV: {props.kegAlcoPerc}</h3>
 				<h3>Pints Left: {props.kegQuantity}</h3>
+				<button onClick={() => onClickBuy(keg.id)}>Buy keg</button>
 				<h3>________________________</h3>
 			</div>
 		</React.Fragment>
@@ -23,7 +25,8 @@ Keg.propTypes = {
 	whenKegClicked: PropTypes.func,
 	id: PropTypes.string,
 	kegBrand: PropTypes.string,
-	kegAlcoPerc: PropTypes.string
+	kegAlcoPerc: PropTypes.string,
+	onClickBuy: PropTypes.func
 };
 
 export default Keg;
