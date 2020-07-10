a009315a1696c20cbf4c2212ddc1137ed875cdf8
brevin <cronkbrevin98@gmail.com>
2020-07-10 13:28:00 -0700

    Selected keg reducer is functional and tested

--- tap-room/src/__tests__/reducers/selected-keg-reducer.test.js
+++ tap-room/src/__tests__/reducers/selected-keg-reducer.test.js
@@ -1,7 +1,54 @@
 import selectedKegReducer from '../../reducers/selected-keg-reducer';
 import * as c from '../../actions/actionTypes';
 
-// Test One
-test('Should return default state if no action type is recognized', () => {
-	expect(selectedKegReducer(null, { type: null })).toEqual(null);
+describe('selectedKegReducer', () => {
+	let action;
+	let defaultData = {
+		1: {
+			kegBrand: 'Corona',
+			kegName: 'Premier',
+			kegPrice: 5,
+			kegAlcoPerc: '4.2',
+			kegQuantity: 5,
+			id: 1,
+			key: 1
+		},
+		2: {
+			kegBrand: 'Elysian Brewery',
+			kegName: 'Space Dust',
+			kegPrice: 5,
+			kegAlcoPerc: '8.3',
+			kegQuantity: 5,
+			id: 2,
+			key: 2
+		}
+	};
+
+	// Test One
+	test('Should return default state if no action type is recognized', () => {
+		expect(selectedKegReducer(null, { type: null })).toEqual(null);
+	});
+
+	// Test Two
+	test('Should select a keg on click', () => {
+		action = {
+			type: c.SELECT_KEG,
+			kegBrand: 'Corona',
+			kegName: 'Premier',
+			kegPrice: 5,
+			kegAlcoPerc: '4.2',
+			kegQuantity: 5,
+			id: 1,
+			key: 1
+		};
+		expect(selectedKegReducer(null, action)).toEqual({
+			kegBrand: 'Corona',
+			kegName: 'Premier',
+			kegPrice: 5,
+			kegAlcoPerc: '4.2',
+			kegQuantity: 5,
+			id: 1,
+			key: 1
+		});
+	});
 });
